df45fee0618cf3a6c1588efa7a5236fd710d63a6
DESKTOP-2K80E4U\Jessie <lujessie950410@gmail.com>
2017-09-19 23:05:45 +0800

    test

---
+++ googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/wirl/Documents/posd_HW/googletest/googlemock
+# Build directory: /home/wirl/Documents/posd_HW/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wirl/Documents/posd_HW/googletest/googletest
+# Build directory: /home/wirl/Documents/posd_HW/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
